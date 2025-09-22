c78bc057869661ec0f3de34b0cc36956d044fbbf
Aman Sahi <128726399+Sriaman245@users.noreply.github.com>
2025-09-22 11:09:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,13 +6,23 @@ import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
 
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT=process.env.PORT || 4000
 const app=express()
 
 app.use(express.json())//all the request will be passed using json method
 app.use(cors())
 await connectDB()
-             
+
+app.get("/googlee4c0920809f819d6.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "googlee4c0920809f819d6.html"));
+});
 
 app.use('/api/user',userRouter)
 app.use('/api/image',imageRouter)
